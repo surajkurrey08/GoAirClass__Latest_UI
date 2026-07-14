41df330590fded8c9fdc05431e3c57584853e819
Demonic-Corpse <adilp37055@gmail.com>
2026-07-14 18:40:58 +0530

    Update navbar logo and styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { Menu, X, User, Bell, ChevronDown, Briefcase } from 'lucide-react'
-import logo from "../assets/logo_new.jpg"
+import logo from "../assets/logo_new4.png"
 import OperatorLoginModal from './OperatorLoginModal'
 import './Navbar.css'
 
