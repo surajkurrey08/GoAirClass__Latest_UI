f88f3d29244bfd48b0845ca3fa64c7340bb70d7f
Rutuja Dhayatidak <itsmedhayatidak@gmail.com>
2026-08-24 13:03:56 +0100

    fix bulk api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    watch: {
+      ignored: ['**/src/assets/data2.json']
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
